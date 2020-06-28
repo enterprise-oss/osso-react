07c4cece001fe632344aa6940d4c5abe6b1d5c1f
Sam Bauch <sbauch@gmail.com>
2020-06-27 20:40:58 -0400

    ts added

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,9 @@ module.exports = {
   },
   extends: [
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended" /
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
