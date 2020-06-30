69affd7074006e1249b1e683bd50f9c222c12d7a
Sam Bauch <sbauch@gmail.com>
2020-06-30 10:06:25 -0400

    publish config

--- rollup-dev.config.js
+++ rollup-dev.config.js
@@ -1,7 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 const input = 'src/index.ts';
 
 const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
