8b97966d021071a8b972915e5ea5281957176e94
Sam Bauch <sbauch@gmail.com>
2020-08-01 10:30:40 -0400

    refactors

--- src/hooks/useOssoFields/index.test.ts
+++ src/hooks/useOssoFields/index.test.ts
@@ -12,10 +12,22 @@ describe('providers', () => {
 
     expect(providers).toMatchSnapshot();
   });
+
+  test('each Identity Provider has a label, value, and icon', () => {
+    const {
+      result: {
+        current: { providers },
+      },
+    } = renderHook(() => useOssoFields());
+
+    providers.map((provider) => {
+      expect(Object.keys(provider).sort).toEqual(['value', 'label', 'icon'].sort);
+    });
+  });
 });
 
 describe('fieldsForProvider', () => {
-  test('it returns the expected fields', () => {
+  test('it returns the expected fields for each provider', () => {
     const {
       result: {
         current: { providers, fieldsForProvider },
--- src/hooks/useOssoFields/index.ts
+++ src/hooks/useOssoFields/index.ts
@@ -1,15 +1,9 @@
-import { OssoProvider, ProviderMap, Providers } from './index.types';
+import { OssoProvider, ProviderMap, ProviderOption, Providers } from './index.types';
 import * as allProviders from './providers';
 
 const providerMap = Object.entries(allProviders).reduce((map, [_key, provider]) => {
   return { ...map, [provider.value as Providers]: provider };
 }, {} as ProviderMap<Providers>);
-
-type ProviderOption = {
-  value: Providers;
-  label: string;
-  icon: string;
-};
 
 const providers: ProviderOption[] = Object.values(allProviders).map((provider) => ({
   value: provider.value,
@@ -21,14 +15,9 @@ const useOssoFields = (): {
   fieldsForProvider: (provider: Providers) => OssoProvider;
   providers: ProviderOption[];
 } => {
-  const fieldsForProvider = (provider: Providers) => {
-    console.log(providerMap);
-    return providerMap[provider];
-  };
-
   return {
     providers,
-    fieldsForProvider,
+    fieldsForProvider: (provider: Providers) => providerMap[provider],
   };
 };
 
