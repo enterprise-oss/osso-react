a8d0213363307664b4e6707b179b847f51c73676
Sam Bauch <sbauch@gmail.com>
2020-08-01 08:25:36 -0400

    actually test, add artifacts

--- src/utils/documentationWriter/index.test.js
+++ src/utils/documentationWriter/index.test.js
@@ -4,11 +4,13 @@ import generateDocumentation from './index';
 
 describe('for Okta', () => {
   test('it writes a PDF', async () => {
-    fs.readFile('__fixtures__/okta.pdf', async (err, data) => {
-      const uint8Array = new Uint8Array(data);
-      const result = await generateDocumentation(uint8Array, { acsUrl: 'https://osso-acs-url.com' });
-      const path = `__artifacts__/okta-docs.pdf`;
-      fs.writeFileSync(path, result);
-    });
+    expect(function write() {
+      fs.readFile('__fixtures__/okta.pdf', async (err, data) => {
+        const uint8Array = new Uint8Array(data);
+        const result = await generateDocumentation(uint8Array, { acsUrl: 'https://osso-acs-url.com' });
+        const path = `__artifacts__/okta-docs.pdf`;
+        fs.writeFileSync(path, result);
+      });
+    }).not.toThrow();
   });
 });
