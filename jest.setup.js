5068d8fb6cc3da9f4da8eaa4a0169656a6e3cdbc
Sam Bauch <sbauch@gmail.com>
2020-08-01 23:53:51 -0400

    cleanup

--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,3 @@
-require('jest-fetch-mock').enableMocks();
+import { enableMocks } from 'jest-fetch-mock';
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
+enableMocks();
