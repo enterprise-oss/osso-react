d508bea00013c95da0b5f9e98ab3742ecb641a44
Sam Bauch <sbauch@gmail.com>
2020-08-26 17:56:30 -0400

    fix download docs button

--- src/components/DownloadDocs/index.tsx
+++ src/components/DownloadDocs/index.tsx
@@ -1,7 +1,7 @@
 import download from 'downloadjs';
 import React, { ReactElement } from 'react';
 
-import { useIdentityProvider } from '~hooks';
+import { useAppConfig, useIdentityProvider } from '~hooks';
 import generateDocumentation, { PDF_VERSION } from '~utils/documentationWriter';
 
 import { IdentityProvider, OssoButtonComponentProps } from './index.types';
@@ -14,14 +14,14 @@ export default function DownloadDocs({
   ButtonComponent: React.FC<OssoButtonComponentProps>;
 }): ReactElement | null {
   const { data } = useIdentityProvider(identityProvider.id);
-
+  const { data: appData } = useAppConfig();
   const downloadDocumentation = async () => {
-    if (!data) return;
+    if (!(data && appData)) return;
     const {
       identityProvider: { service, domain },
     } = data;
     const template = await fetch(`/pdfv${PDF_VERSION}/${service.toLowerCase()}.pdf`).then((res) => res.arrayBuffer());
-    const pdf = await generateDocumentation(template, data.identityProvider);
+    const pdf = await generateDocumentation(template, data.identityProvider, appData.appConfig);
     download(pdf, `${service} SAML setup - ${domain}.pdf`, 'application/pdf');
   };
 
