3ff4cdd46fb7feebb35adba159253efcc7d43ab8
Sam Bauch <sbauch@gmail.com>
2020-09-28 12:12:08 -0400

    fix test

--- src/hooks/useIdentityProvider/__fixtures__.ts
+++ src/hooks/useIdentityProvider/__fixtures__.ts
@@ -4,6 +4,7 @@ export default {
     service: 'OKTA',
     domain: 'intro.com',
     acsUrl: 'http://localhost:9292/auth/saml/b466d079-4211-460e-8d4f-62ca03626486/callback',
+    acsUrlValidator: 'http://localhost:9292/auth/saml/b466d079\\-4211\\-460e\\-8d4f\\-62ca03626486/callback',
     ssoCert: null,
     ssoUrl: null,
     status: 'Pending',
