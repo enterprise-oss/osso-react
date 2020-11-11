3f1158f6970260e3b69de3c0cffd1afd75554b21
Sam Bauch <sbauch@gmail.com>
2020-11-10 19:20:10 -0500

    update fixture

--- src/hooks/useIdentityProvider/__fixtures__.ts
+++ src/hooks/useIdentityProvider/__fixtures__.ts
@@ -9,6 +9,10 @@ export default {
     ssoIssuer: 'http://localhost:9292/intro.com',
     ssoUrl: null,
     status: 'Pending',
+    oauthClient: {
+      id: 'b466d079-4211-490e-8d4f-62ca03626486',
+      name: 'Production',
+    },
     __typename: 'IdentityProvider',
   },
 };
