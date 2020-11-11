ab219eb35aab34ddfb932f8a4681eea8f776acd7
Sam Bauch <sbauch@gmail.com>
2020-11-10 19:35:53 -0500

    Add OauthClient to idp queries (#33)
    Supports displaying Oauth Client for IDP. TODO: N+1 on oauth clients

--- src/hooks/useEnterpriseAccount/__fixtures__.ts
+++ src/hooks/useEnterpriseAccount/__fixtures__.ts
@@ -19,6 +19,10 @@ export default {
         ssoUrl: null,
         ssoIssuer: `http://localhost:9292/${domain}`,
         status: 'Pending',
+        oauthClient: {
+          id: 'b466d079-4211-490e-8d4f-62ca03626486',
+          name: 'Production',
+        },
       },
     ],
     createdAt: '2020-08-12T18:40:00+0000',
