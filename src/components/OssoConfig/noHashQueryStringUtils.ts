80a26a0befd488182f3e1303d08328a4de65c3b6
Sam Bauch <sbauch@gmail.com>
2020-12-02 13:57:51 -0500

    rm app auth stuff

--- src/components/OssoConfig/noHashQueryStringUtils.ts
+++
@@ -1,8 +0,0 @@
-import { BasicQueryStringUtils, LocationLike, StringMap } from '@openid/appauth';
-
-export class NoHashQueryStringUtils extends BasicQueryStringUtils {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  parse(input: LocationLike, useHash: boolean): StringMap {
-    return super.parse(input, false /* never use hash */);
-  }
-}
